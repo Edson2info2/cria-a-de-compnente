3bdc2d4e33248a20c4921fde555d952e52ea67cb
Edson2info2 <estevanyubo@gmail.com>
2022-12-16 23:51:52 -0300

    segunda commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhO from "@/components/CabecalhO.vue";
+import PrincipaL from "@/components/PrincipaL.vue";
+import RodapE from "@/components/RodapE.vue";
+export default {
+  components: { MenuLateral, CabecalhO, PrincipaL, RodapE },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabecalhO />
+  <PrincipaL />
+  <RodapE />
+</template>
+<style></style>
---
+++ src/components/PrincipaL.vue
@@ -0,0 +1,23 @@
+<script>
+import CompCaixas from "@/components/CompCaixas.vue";
+export default {
+  components: { CompCaixas },
+  data() {
+    return {
+      musicas1: [],
+      musicas2: [],
+    };
+  },
+};
+</script>
+<template>
+  <main id="content">
+    <CompCaixas titulo="The Clash" class="one" />
+    <CompCaixas titulo="Nirvana" />
+    <CompCaixas titulo="Pentakill" class="one" />
+    <CompCaixas titulo="The Beatles" class="one" />
+    <CompCaixas titulo="Peatch" />
+    <CompCaixas titulo="One Pice" class="one" />
+  </main>
+</template>
+<style></style>
